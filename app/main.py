7ed15593b38f8baa91e52a542d50a442db626307
Felipe Morales figueroa <fe.moralesf@duocuc.cl>
2025-05-31 22:43:18 -0400

    CORS añadido

--- app/main.py
+++ app/main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from app.routers import productos
 
 app =  FastAPI(
@@ -6,5 +7,20 @@ app =  FastAPI(
     version = "1.0",
     descripcion= "API de gestion exclusivamente de productos usando DB oracle"
 ) 
+
+# Configuración de CORS
+origins = [
+    "http://localhost:8000",  # Para Django en desarrollo
+    "http://127.0.0.1:8000",
+
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.include_router(productos.router)
